f2c5f2772b4d488ff66763329c831ad9c48a49b5
mehrshad <109306196+mhrshxd@users.noreply.github.com>
2025-03-01 10:53:14 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Button click effect for animated box
 document.getElementById("coolButton").addEventListener("click", function() {
     let box = document.getElementById("box");
     box.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -9,22 +8,6 @@ document.getElementById("coolButton").addEventListener("click", function() {
         box.style.transform = "none";
     }, 1000);
 });
-
-// Mouse movement for animated box
-if (!window.matchMedia("(max-width: 768px)").matches) {
-    window.addEventListener("mousemove", function(e) {
-        let box = document.getElementById("box");
-        let boxWidth = box.offsetWidth;
-        let boxHeight = box.offsetHeight;
-        box.style.left = (e.clientX - boxWidth / 2) + "px";
-        box.style.top = (e.clientY - boxHeight / 2) + "px";
-        box.style.transition = "background-color 0.5s";
-        box.style.opacity = "0.651";
-        box.style.backgroundColor = "#3498db";
-    });
-}
-
-// Particle effect
 function createParticle() {
     const particle = document.createElement('div');
     particle.classList.add('particle');
